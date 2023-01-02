d960c1c0ff4b4d01c4b97b9da8583991346dd771
seungbeen <seungbin803@gmail.com>
2023-01-02 11:08:41 +0900

    포인터 예제

--- CosPro_day1/포인터.c
+++ CosPro_day1/포인터.c
@@ -9,4 +9,24 @@ void main() {
 	printf("number의 값 %d\n", number);
 	*ptr = *ptr + 5;
 	printf("number의 값 : %d\n", number);
+
+	// 포인터 변수의 크기
+	// ==> sizeof (포인터 변수) : 주소값 크기
+	// sizeof(*포인터 변수) : 포인터 변수가 가리키고 있는 대상의 자료형
+
+	// p45 포인터의 표현과 배열 표현
+	int arr[10] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+	int* pArr;
+	int i;
+	pArr = &arr[0];
+	for (i = 0; i < 10; i++) {
+		printf("%p: %d\n", &arr[i], arr[i]);
+	}
+
+	for (i = 0; i < 10; i++) {
+		printf("%p: %d\n", pArr + i, *(pArr + i));
+		// 포인터변수 + 위치 : 위치에 해당하는 주소값
+		// *(포인터변수 + 위치) : 위치에 해당하는 값을 리턴
+	}
+
 }
